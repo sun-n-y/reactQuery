30d1bbd961bf7093edecdc65ea513d8c15aec963
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-17 12:47:25 -0500

    add first react query

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,11 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
 import SingleItem from './SingleItem';
+import customFetch from './utils';
+
 const Items = ({ items }) => {
+  const result = useQuery({
+    queryKey: ['tasks'],
+    queryFn: () => customFetch.get('/'),
+  });
+  console.log(result);
+
   return (
-    <div className='items'>
+    <div className="items">
       {items.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
   );
 };
+
 export default Items;
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customFetch = axios.create({
-  baseURL: 'https://local:5000/api/tasks',
+  baseURL: 'http://localhost:5000/api/tasks',
 });
 
 export default customFetch;
