c48c85d43268342e360bf3d722b91a2f6c911ee4
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-18 14:25:05 -0500

    add custom hook for delete and edit tasks

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,32 +1,9 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import customFetch from './utils';
-import { toast } from 'react-toastify';
+import { useDeleteTask, useEditTask } from './reactQueryCustomHooks';
 
 const SingleItem = ({ item }) => {
-  const queryClient = useQueryClient();
+  const { editTask } = useEditTask();
 
-  const { mutate: editTask } = useMutation({
-    mutationFn: ({ taskId, isDone }) =>
-      customFetch.patch(`/${taskId}`, { isDone }),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['tasks'] });
-      toast.success('task edited');
-    },
-    onError: (error) => {
-      toast.error(error.response.data.msg);
-    },
-  });
-
-  const { mutate: deleteTask, isLoading } = useMutation({
-    mutationFn: (taskId) => customFetch.delete(`/${taskId}`),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['tasks'] });
-      toast.success('task deleted');
-    },
-    onError: (error) => {
-      toast.error(error.response.data.msg);
-    },
-  });
+  const { isLoading, deleteTask } = useDeleteTask();
 
   return (
     <div className="single-item">
--- src/reactQueryCustomHooks.jsx
+++ src/reactQueryCustomHooks.jsx
@@ -1,5 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import customFetch from './utils';
+import { toast } from 'react-toastify';
 
 export const useFetchTasks = () => {
   const { isLoading, data, isError, error } = useQuery({
@@ -9,6 +10,37 @@ export const useFetchTasks = () => {
   return { isLoading, isError, data };
 };
 
-export const useCreateTasks = () => {};
-export const useEditTasks = () => {};
-export const useDeleteTasks = () => {};
+export const useCreateTask = () => {};
+
+export const useEditTask = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: editTask } = useMutation({
+    mutationFn: ({ taskId, isDone }) =>
+      customFetch.patch(`/${taskId}`, { isDone }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['tasks'] });
+      toast.success('task edited');
+    },
+    onError: (error) => {
+      toast.error(error.response.data.msg);
+    },
+  });
+  return { editTask };
+};
+
+export const useDeleteTask = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteTask, isLoading } = useMutation({
+    mutationFn: (taskId) => customFetch.delete(`/${taskId}`),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['tasks'] });
+      toast.success('task deleted');
+    },
+    onError: (error) => {
+      toast.error(error.response.data.msg);
+    },
+  });
+  return { deleteTask, isLoading };
+};
